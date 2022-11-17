1dbb978942551fdd05f708efccf8cccec1ebb172
miguel silva <miguelvdsilva@gmail.com>
2022-11-17 13:01:11 +0000

    lunch update

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,10 @@
+# frozen_string_literal: true
+
 class Movie < ApplicationRecord
-has_many :bookmarks
+  has_many :bookmarks
 
-validates :title, presence: true
-validates :title, uniqueness: true
+  validates :title, presence: true
+  validates :title, uniqueness: true
 
-validates :overview, presence: true
-
-
+  validates :overview, presence: true
 end
